acb496f546af83715c519d03eee3611197c56bb8
Maria Bazhenova <yankeevsdixie@gmail.com>
2025-07-11 20:18:06 +0300

    created index.js inside images folder for easy export

---
+++ src/assets/images/index.js
@@ -0,0 +1,28 @@
+import aboutImg from "./about-img.webp";
+import client1 from "./client1.webp";
+import client2 from "./client2.webp";
+import client3 from "./client3.webp";
+import mainImg from "./main-img.webp";
+import portfolio1 from "./portfolio-1.webp";
+import portfolio2 from "./portfolio2.webp";
+import portfolio3 from "./portfolio3.webp";
+import portfolio4 from "./portfolio4.webp";
+import portfolio5 from "./portfolio5.webp";
+import portfolio6 from "./portfolio6.webp";
+import Shape from "./shape.png";
+
+
+export {
+    aboutImg,
+    client1,
+    client2,
+    client3,
+    mainImg,
+    portfolio1,
+    portfolio2,
+    portfolio3,
+    portfolio4,
+    portfolio5,
+    portfolio6,
+    Shape
+}
