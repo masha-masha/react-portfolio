7b8463c78f388e2dfb48762e04f8b92094f8567b
Maria Bazhenova <yankeevsdixie@gmail.com>
2025-07-13 18:35:58 +0300

    added icon for title

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,10 +3,26 @@ import "./About.css";
 
 const About = () => {
   return (
-    <div className="about">
-       <div>About</div>
-       <img src={aboutImg} alt="about" />
-    </div>
+    <section className="about section" id="about">
+       <div className="container flex-container">
+        <h2 className="inner-title">About me</h2>
+        <h3 className="inner-second-title">About me</h3>
+        <div className="about-wrapper">
+          <div className="about-img">
+            <img src={aboutImg} alt="about" />
+          </div>
+          <div className="about-info">
+            <div className="description">
+              <h3>I'm Ilay</h3>
+              <h4>A Lead <span>Front-end Developer</span></h4>
+              <p>
+                I design and develop services for customers
+              </p>
+            </div>
+          </div> 
+        </div>
+       </div>
+    </section>
   )
 }
 
