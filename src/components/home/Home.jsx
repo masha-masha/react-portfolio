1831d19af48ce0f749e749d98ba040a12e9a390b
Maria Bazhenova <yankeevsdixie@gmail.com>
2025-07-13 18:17:58 +0300

    added home section and styles for home section

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,11 +1,40 @@
 import "./Home.css";
 import { mainImg } from "../../assets/images";
+import { FaFacebookF,  FaInstagram, FaGithub} from "react-icons/fa";
+import { FaCircleArrowRight, FaArrowDownLong } from "react-icons/fa6";
 
 const Home = () => {
   return (
     <section className="home" id="home">
-      <h2>Home</h2>
-      <img src={mainImg} alt="main img" />
+      <div className="container home-wrapper">
+        <div className="media-icons">
+          <a href="">
+            <FaFacebookF/>
+          </a>
+          <a href="">
+            <FaInstagram/>
+          </a>
+          <a href="">
+            <FaGithub/>
+          </a>
+        </div>
+        <div className="home-info">
+          <h1>Hi, I am Ilay</h1>
+          <h3>Front-end Developer</h3>
+          <p>I create stunning websites for your busness, Highly experience in web design and development</p>
+          <a href="" className="home-info-link inner-info-link">
+            <span>Contact me</span>
+            <FaCircleArrowRight />
+          </a>
+        </div>
+        <div className="home-img">
+          <img src={mainImg} alt="main image" />
+        </div>
+      </div>
+      <a href="#about" class="scroll-down">
+        Scroll down
+        <FaArrowDownLong/>
+      </a>
     </section>
   )
 }
